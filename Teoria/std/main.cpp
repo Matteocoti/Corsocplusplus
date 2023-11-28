e1ecd33347471bad6432055309ed537fc1cb9e32
matteo <cotifavamatteo@gmail.com>
2023-11-28 14:35:02 +0100

    Pulito codice

--- Teoria/std/main.cpp
+++ Teoria/std/main.cpp
@@ -1,78 +1,12 @@
-#include <fstream>
-#include <utility>
 #include <vector>
 #include <algorithm>
-
+#include <fstream>
 // Per standard output invece che su file
 #include <iostream> 
 
 // Nella libreria iterator c'é la classe che adatta uno stream in modo da poterlo gestire come 
 // se fosse una classe di elementi
 #include <iterator>
-
-/**
- * Tre categorie di cose dentro la libreria: 
- * - container -> classi che rappresentano delle strutture dati (esempio vector, list, set)
- * - iteratori -> meccanismi standardizzati per ciclare su i contenitori
- * - algoritmi -> basati sugli iteratori su dei contenitori (molto efficienti)
-*/
-
-/**
- * Vettori vs liste vs set:
- * - liste più comode solo in alcuni casi e hanno senso solo se gli algoritmi gestiscono tutta la lista
- * - il vettore molto più veloce (se devo accedere al 50esimo elemento, so già dove trovarlo, mentre nelle liste no)
- * - set é più veloce per cercare un elemento all'interno perché gestisce gli elementi tramite un labero binario. Quindi faccio 
- * meno confronti (é fatto apposta).
- * 
-*/
-
-/**
- *  Nella libreria standard esistono gli adaptor che sono degli oggetti che ci permettono di trattare delle classi 
- * con degli iteratori
- * /
-
-// template<typename T>
-// bool scrivi_vettore(const std::string& fileName, const std::vector<T>& v){
-
-//     std::ofstream os(fileName); 
-
-//     if (!os) return false; 
-    
-//     // Si può vedere il codice sorgente perché é una funzione a template, quindi non si può compilare prima
-
-//     // Se v fosse una lista, il funzionamento sarebbe lo stesso a meno del tipo dell'iteratore
-//     auto it_stop = v.end();
-
-//     // Prendi v e chiama begin e hai l'iteratore, poi ad ogni ciclo chiami il next e lo fai fino ad end.
-//     // La forma si chiama Range Based For 
-//     // x é solo il nome che diamo per accedere il nome corrente di v
-//     for (const auto& x : v) {
-//         os << x << '\n';
-//     }
-
-//     return true;
-// }
-
-/**
- * La funzione di prima non va bene, perché nel momento in cui desidero stampare su standard output non va più bene.
- * Dovrei fare un overload.
- * 
- * std::ostream classe generica di stream di output
-*/
-template<typename T>
-void scrivi_vettore(std::ostream& os, const std::vector<T>& v){
-
-    // Si può vedere il codice sorgente perché é una funzione a template, quindi non si può compilare prima
-    // Se v fosse una lista, il funzionamento sarebbe lo stesso a meno del tipo dell'iteratore
-    auto it_stop = v.end();
-
-    // Prendi v e chiama begin e hai l'iteratore, poi ad ogni ciclo chiami il next e lo fai fino ad end.
-    // La forma si chiama Range Based For 
-    // x é solo il nome che diamo per accedere il nome corrente di v
-    for (const auto& x : v) {
-        os << x << '\n';
-    }
-}
 
 int main(int argc, char** argv) {
 
