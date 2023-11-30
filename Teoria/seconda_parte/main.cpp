ca848b91a27735b3737d5bb533a7a414963f91a1
matteo <cotifavamatteo@gmail.com>
2023-11-30 10:32:24 +0100

    Aggiunto operatore += e corretto operatore +

--- Teoria/seconda_parte/main.cpp
+++ Teoria/seconda_parte/main.cpp
@@ -40,23 +40,7 @@ public:
         this->den_ = den / MCD;
     }
 
-    rational operator+(const rational &b) const
-    {
-        // Porto a e b allo stesso denominatore
-        // Calcolo il minimo comune multiplo (prodotto dei numeri diviso MCD)
-        int mcm = this->den_ / gcd(this->den_, b.den_) * b.den_;
-        // Porto a al denominatore mcm
-        // Porto b al denominatore mcm
-        // Sommo i numeratori
-        int s = this->num_ * mcm / this->den_ + b.num_ * mcm / b.den_;
-        // Riduco ai minimi termini
-        // divido numeratore e denominatore per il loro MCD
-        int mcd = gcd(abs(s), mcm);
-
-        return rational{s / mcd, mcm / mcd};
-    }
-
-    rational &operator+(const rational &b)
+    rational& operator+=(const rational &b) 
     {
         // Porto a e b allo stesso denominatore
         // Calcolo il minimo comune multiplo (prodotto dei numeri diviso MCD)
@@ -73,29 +57,35 @@ public:
         this->den_ = mcm / mcd;
         return *this;
     }
-
-
-    bool operator==(const rational &b) const
+    // friends defined inside class body are inline and are hidden from non-ADL lookup
+    // passing lhs by value helps optimize chained a+b+c
+    // otherwise, both parameters may be const references
+    friend rational operator+(rational lhs, const rational& rhs) 
     {
-        return this->num_ == b.num_ && this->den_ == b.den_;
+        lhs += rhs; // reuse compound assignment
+        return lhs; // return the result by value (uses move constructor)
     }
 
-    bool operator<(const rational &b) const
+    friend bool operator==(const rational &lhs, const rational &rhs) 
     {
-
-        int mcm = this->den_ / gcd(this->den_, b.den_) * b.den_;
-
-        return (this->num_ * (mcm / this->den_)) < (b.num_ * (mcm / b.den_));
+        return lhs.num_ == rhs.num_ && lhs.den_ == rhs.den_;
     }
 
-    bool operator>(const rational &b) const 
+    // La prassi é definire gli operatori di confronto come funzioni free-standing non come 
+    // metodi.
+    friend bool operator<(const rational &lhs, const rational &rhs)
     {
-        return (!(*this<b) && !(*this==b));
+        int mcm = lhs.den_ / gcd(lhs.den_, rhs.den_) * rhs.den_;
+
+        return (lhs.num_ * (mcm / lhs.den_)) < (rhs.num_ * (mcm / rhs.den_));
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const rational &r); 
-
-    friend std::istream &operator>>(std::istream &is, rational& r);
+    friend inline bool operator> (const rational& lhs, const rational& rhs) { return rhs < lhs; }
+    friend inline bool operator<=(const rational& lhs, const rational& rhs) { return !(lhs > rhs); }
+    friend inline bool operator>=(const rational& lhs, const rational& rhs) { return !(lhs < rhs); }
+    friend inline bool operator!=(const rational& lhs, const rational& rhs) { return !(lhs == rhs); }
+    friend std::ostream &operator<<(std::ostream &os, const rational &r);
+    friend std::istream& operator>>(std::istream &is, rational& r);
 
 };
 // In C gli operatori sono metodi del primo oggetto, ma se non sono presenti, possono essere una funzione esterna.
@@ -119,16 +109,19 @@ std::istream& operator>>(std::istream &is, rational& r)
     //     is.setstate(std::ios_base::iostate::_S_failbit);
     //     return is;
     // }
-
-    is >> tmp;
-
-    // Controllo se é presente uno spazio vuoto
-    if (tmp != '/') {
-        is.unget();
+    if (is.eof()) {
         r = rational(num);
         return is;
     }
 
+    is >> tmp;
+    // Controllo se é presente uno spazio vuoto
+    if ((tmp != '/') || is.eof()) {
+        is.unget();
+        r = rational(num);
+        return is;
+    }
+    
     if (!(is >> den)) return is;
 
     r = rational(num, den);
